8464bd10e32433e2379dd34612252e9fc59ef636
MajorTomAW <tom.wrnr.private@gmail.com>
2024-12-29 17:14:39 +0100

    Initial modules

---
+++ Source/GameFeatureDataTablesEditor/Private/GameFeatureDataTablesEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "GameFeatureDataTablesEditor.h"
+
+#define LOCTEXT_NAMESPACE "FGameFeatureDataTablesEditorModule"
+
+void FGameFeatureDataTablesEditorModule::StartupModule()
+{
+    
+}
+
+void FGameFeatureDataTablesEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FGameFeatureDataTablesEditorModule, GameFeatureDataTablesEditor)
